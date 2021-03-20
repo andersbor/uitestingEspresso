ad58cf9054ab6dcec04d563d7d92046ee8d9831f
Anders Børjesson <anbo@zealand.dk>
2021-03-20 20:14:34 +0100

    gradle update

--- app/src/androidTest/java/dk/easj/anbo/uitesting/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/dk/easj/anbo/uitesting/ExampleInstrumentedTest.java
@@ -1,23 +1,20 @@
 package dk.easj.anbo.uitesting;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,14 +23,6 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-
-   /* @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-        assertEquals("dk.easj.anbo.uitesting", appContext.getPackageName());
-    }*/
-
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule(MainActivity.class);
@@ -54,7 +43,7 @@ public class ExampleInstrumentedTest {
 
         onView(withId(R.id.anotherTextView)).check((matches(withText("Another"))));
 
-        pressBack();
+        pressBack(); // press the back button
         onView(withText("2")).check(matches(isDisplayed()));
     }
 }
